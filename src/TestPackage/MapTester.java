5271d0c0141ac9b372264d9144c82884d2777ddc
RobClinch <r.clinch@hotmail.nl>
2015-02-04 15:47:42 +0100

    Added Test Package

---
+++ src/TestPackage/MapTester.java
@@ -0,0 +1,15 @@
+package TestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MapTester {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
