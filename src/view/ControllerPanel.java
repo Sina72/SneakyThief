b13fd02a243b09fb55bcca21e96b2f9d52d72221
RobClinch <r.clinch@hotmail.nl>
2015-02-08 01:22:13 +0100

    Added small changes in controller panel

--- src/view/ControllerPanel.java
+++ src/view/ControllerPanel.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -13,6 +15,16 @@ public class ControllerPanel extends JPanel {
 
 	public ControllerPanel()
 	{
+		//CREATE ELEMENTS
+		GridLayout controllerlayout = new GridLayout(3,1);
 		JButton startgame = new JButton("Begin Simulation");
+		JButton pausegame = new JButton("Pause Simulation");
+		JButton stopgame = new JButton("Stop Simulation");
+		
+		//ADD ELEMENTS
+		this.setLayout(controllerlayout);
+		this.add(startgame);
+		this.add(pausegame);
+		this.add(stopgame);
 	}
 }
