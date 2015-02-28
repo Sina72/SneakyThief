0eef7cbb35f87fa011063dec99988bbcbc85420d
RobClinch <r.clinch@hotmail.nl>
2015-02-28 22:10:46 +0100

    Begun on the map drawing, need to be able to access placement list form map. Created MapDrawer class as JComponent to add to the MapPanel. Not 100% sure how to create test for each of these classes seperately.

--- src/view/MapPanel.java
+++ src/view/MapPanel.java
@@ -1,8 +1,12 @@
 
 package view;
 
+import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -17,7 +21,12 @@ public class MapPanel extends JPanel implements Observer {
 	
 	//THIS PANEL PLACED ON THE RIGHT OF THE BORDER LAYOUT
 	private Map map;
+
 	
+	public MapPanel()
+	{
+		MapDrawer drawer = new MapDrawer();
+	}
 	
 	@Override
 	public void update(Observable o, Object arg) {
@@ -25,18 +34,26 @@ public class MapPanel extends JPanel implements Observer {
 
 	}
 	
-	public void draw()
+	public void paint(Graphics g, int x, int y)//x/y are end points of line
 	{
-		/*for each MapPlacement we must draw it, essentially drawing the map
-		 * get map
-		 * get placements
-		 * draw placements
-		*/
-	
+		g.setColor(Color.BLACK);
+		g.drawLine(10, 10, 50, 50);
 	}
 	
 	public void drawShape(Shape newshape)
 	{
+		
+	}
+	public void drawShape(Rectangle shape, Graphics g)
+	{
+		int x = (int)shape.getX();
+		int y = (int) shape.getY();
+		int width = (int) shape.getWidth();
+		int height = (int) shape.getHeight();
+		g.drawLine(x,y,x+width,y);
+		g.drawLine(x,y,x,y+height);
+		g.drawLine(x+width,y,x+width,y+height);
+		g.drawLine(x,y+height,x+width,y+height);
 		
 	}
 }
