9ae66015f09057c93ebe6c44bd663700207ff15b
RobClinch <r.clinch@hotmail.nl>
2015-03-04 12:17:41 +0100

    More Drawer stuff, uncommented a lotta errors

--- src/view/MapDrawer.java
+++ src/view/MapDrawer.java
@@ -3,6 +3,7 @@ package view;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.List;
 
 import javax.swing.JComponent;
 
@@ -18,26 +19,43 @@ public class MapDrawer extends JComponent
 	{
 		Graphics2D g2 = (Graphics2D) g;
 		
-		/*for(int i =0; i<placements.size;i++)
-		 *{
-		 *	currentplacement = placements.get(i);
-		 *	if(currentplacement is certain shape)
-		 *	{
-		 *		do a draw method
-		 *	}
-		 *	if(currentplacement is a polygon)
-		 *	{
-		 *		for(each coordinate)
-		 *		{
-		 *			draw line to previous coordinate
-		 *		}
-		 *	}
-		 *	if(currentplacement is a line)
-		 *	{
-		 *		g2.draw(new Line2D.Double(firstcoordinate, secondcoordinate)
-		 *	}
-		 *}
-		*/
+		//TODO: Get the map and the placements
+		//TODO: Finish draw methods
+		for(int i =0; i<placements.size;i++)
+		{
+		 	currentplacement = placements.get(i);
+		 	if(currentplacement is certain shape)
+		 	{
+		 		do a certain draw method
+		 	}
+		 	if(currentplacement instanceof Polygon && !currentplacement instanceof Line)
+		 	{
+		 		new List<> listofpoints = currentplacement.toLines();
+		 		for(int i =0;i>listofpoints.size;i++)
+		 		{
+					Double startx,starty,endx,endy;
+	 				startx = listofpoints[i].getX();
+		 			starty = listofpoints[i].getY();
+		 			if(i == listofpoints.size)
+		 			{
+		 				endx = listofpoints[i+1].getX();
+						endy = listofpoints[i+1].getY();
+	 				}
+		 			else
+		 			{	
+		 				endx = listofpoints[0].getX();
+	 					endy = listofpoints[0].getY();
+	 				}
+		 				
+					g2.draw(new Line2D.Double(startx,starty,endx,endy));
+	 			}
+		 	}
+		 	if(currentplacement instanceof Line)
+		 	{
+		 		g2.draw(new Line2D.Double(firstcoordinate, secondcoordinate)
+		 	}
+		}
+		
 		
 		
 		
