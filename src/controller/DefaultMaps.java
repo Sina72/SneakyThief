c07a915e0e5b78728c711515d6b45a48cff4b276
Sharonneke95 <sharonneke95@hotmail.com>
2015-03-09 22:04:09 +0100

    One default map

--- src/controller/DefaultMaps.java
+++ src/controller/DefaultMaps.java
@@ -13,12 +13,21 @@ public class DefaultMaps {
 		mapBuilder = new MapBuilder(200,200);
 		
 		try {
-			mapBuilder.addWall(
-					new Coordinate(0,0), 
-					new Coordinate(200,5)
-					);
+			//mapBuilder.addWall(Coordinate leftBottom, Coordinate rightTop)
+			mapBuilder.addWall(new Coordinate(10,10),new Coordinate(10,190));
+			mapBuilder.addWall(new Coordinate(10,190), new Coordinate(130,190));
+			mapBuilder.addWall(new Coordinate(70,170), new Coordinate(180,170));
+			mapBuilder.addWall(new Coordinate(180,70), new Coordinate(180,170));
+			mapBuilder.addWall(new Coordinate(170,30), new Coordinate(170,130));
+			mapBuilder.addWall(new Coordinate(70,30), new Coordinate(170,30));
+			mapBuilder.addWall(new Coordinate(30,40), new Coordinate(130,40));
+			mapBuilder.addWall(new Coordinate(30,40), new Coordinate(30,80));
 			
-			mapBuilder.addGuard(new Coordinate(30,30));
+			mapBuilder.addWall(new Coordinate(75,85), new Coordinate(75,115));
+			mapBuilder.addWall(new Coordinate(85,75), new Coordinate(115,75));
+			mapBuilder.addWall(new Coordinate(85,125), new Coordinate(115,125));
+			mapBuilder.addWall(new Coordinate(125,85), new Coordinate(125,115));
+			//mapBuilder.addGuard(new Coordinate(30,30));
 			
 			//etcetera
 			
